0729fe9862fda3b1b0b74bf7ac77d9d4ebb93361
Victor Kudelin <58315363+K-vic98@users.noreply.github.com>
2021-01-19 08:08:46 +0300

    made the configuration of fighters

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private float _secondsBetweenShot;
+
+    private float _lastShootTime;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void TakeDamage(int damage)
+    {
+        _health -= damage;
+    }
+}
